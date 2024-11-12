97b3b539a16b31fbf76b0533950d8841c3ce89e2
alwindonald <alwindonald4004@gmail.com>
2024-11-12 21:43:38 +0530

    inheritance

---
+++ Javainheritance/src/com/Inheritance/SingleInheritance/SingleInheritanceExecuter.java
@@ -0,0 +1,12 @@
+package com.Inheritance.SingleInheritance;
+
+public class SingleInheritanceExecuter {
+
+	public static void main(String[] args) {
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "CityCollege");
+		
+		System.out.println(student);
+
+	}
+
+}
